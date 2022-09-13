5b9e26c65a79f6611a127a87e4201c2ea2d97abc
Christophe Mion <37846046+cmion1@users.noreply.github.com>
2022-09-12 21:59:01 -0400

    Add files via upload

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
-#guessinggame.sh
-#author : Christophe Mion
-
-function guessinggame {
-clear && cat << "ART"
+#!/usr/bin/env bash #guessinggame.sh #author : Christophe Mion
+count=$(ls -1 | wc -l)
+while [[ $count -gt 0 ]]; do
+sleep 1 && clear && cat << "ART"
 _____                     _               _____
 |  __ \                   (_)             |  __ \
 | |  \/_   _  ___  ___ ___ _ _ __   __ _  | |  \/ __ _ _ __ ___   ___
@@ -13,12 +11,10 @@ _____                     _               _____
                                    __/ |
                                   |___/
 ART
-echo "" && echo "Your current directory is : " && pwd && echo ""
-count=$(ls | wc -l)
-# prompting the user
-echo "How much files would you guess are in this folder ?" && read entry
+sleep 1 && echo "This is the current folder" && sleep 1 && pwd && sleep 1 && echo "How much files would you guess are in this folder ?"
+read entry && sleep 2
 if [[ $count -eq $entry ]]
-then clear && cat << "congrats"
+then clear && sleep 2 && cat << "congrats"
 _____                             _
 /  __ \                           | |
 | /  \/ ___  _ __   __ _ _ __ __ _| |_ ___
@@ -28,19 +24,25 @@ _____                             _
                    __/ |
                   |___/
 congrats
-  sleep 3 && clear
-fi }
-guessinggame
-while [[ $count -ne $entry ]];
-do clear && cat << "tryagain"
-  _____             ___              _
-  |_   _|           / _ \            (_)
-   | |_ __ _   _  / /_\ \ __ _  __ _ _ _ __
-   | | '__| | | | |  _  |/ _` |/ _` | | '_ \
-   | | |  | |_| | | | | | (_| | (_| | | | | |
-   \_/_|   \__, | \_| |_/\__, |\__,_|_|_| |_|
-            __/ |         __/ |
-           |___/         |___/
-tryagain
-sleep 3 && guessinggame
+sleep 2 && clear && exit
+elif [[ $count -gt $entry ]]
+then clear && sleep 1 && cat << "mo" && sleep 2
+_   _       _         _____                        _
+| \ | |     | |       |  ___|                      | |
+|  \| | ___ | |_      | |__ _ __   ___  _   _  __ _| |__
+| . ` |/ _ \| __|     |  __| '_ \ / _ \| | | |/ _` | '_ \
+| |\  | (_) | |_      | |__| | | | (_) | |_| | (_| | | | |
+\_| \_/\___/ \__|     \____/_| |_|\___/ \__,_|\__, |_| |_|
+                                              __/ |
+                                             |___/
+mo
+else clear && sleep 1 && cat << "lss" && sleep 2
+  _____              ___  ___           _
+ |_   _|             |  \/  |          | |
+   | | ___   ___     | .  . |_   _  ___| |__
+   | |/ _ \ / _ \    | |\/| | | | |/ __| '_ \
+   | | (_) | (_) |   | |  | | |_| | (__| | | |
+   \_/\___/ \___/    \_|  |_/\__,_|\___|_| |_|
+lss
+fi
 done
